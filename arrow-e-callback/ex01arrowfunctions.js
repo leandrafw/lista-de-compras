6f845107f00dcf417ad543c1b3e80cf9ab7932b9
Leandra Ferrari <lehfweber@gmail.com>
2021-06-17 22:58:32 -0300

    exercicio funções avançadas

--- arrow-e-callback/ex01arrowfunctions.js
+++ arrow-e-callback/ex01arrowfunctions.js
@@ -1,6 +1,5 @@
 // Aula 07 - 17/06/2021
 // microdesafio arrow function
-
 console.log(somar());
 
 let print = mensagem => console.log(mensagem);
--- arrow-e-callback/excallbacks.js
+++ arrow-e-callback/excallbacks.js
@@ -1,6 +1,5 @@
 // Aula 07 - 17/06/2021
 // microdesafio callback
-
 let acaoCarro = (callback) => {
 console.log("o carro " + callback);
 }
