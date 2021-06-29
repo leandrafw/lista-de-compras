335668f1050c970c3e73e307d1f22b49bfae2d90
Leandra Ferrari <lehfweber@gmail.com>
2021-06-29 16:44:38 -0300

    exercicios importação modulos

--- importação-modulo/app.js
+++ importação-modulo/app.js
@@ -2,6 +2,6 @@ const fatorial = require('./node_modules/fatorial/fatorial')
 
 console.log(fatorial.calcula(6));
 
-const fibo = require('fibonacci_sequence/lib')
+const fibo = require('./node_modules/fibonacci_sequence/lib')
 
 console.log(fibo.fibonacciSequence(8));
